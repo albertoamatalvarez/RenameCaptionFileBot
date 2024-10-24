cd6690ba1ae7532d6136478203d4276e7dbc2863
Alberto <albertoamatalvarez@gmail.com>
2024-10-24 12:44:11 +0200

    Added method handle_photo to handler.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from decouple import config
 from telegram.ext import ApplicationBuilder, MessageHandler, filters, CommandHandler
 
-from handler.handler import start, handle_video
+from handler.handler import start, handle_video, handle_photo
 
 # Define tu token de bot
 TOKEN = config('TELEGRAM_TOKEN')
@@ -13,5 +13,7 @@ if __name__ == '__main__':
     app.add_handler(CommandHandler('start', start))
     # Añadimos el manejador para archivos de tipo video
     app.add_handler(MessageHandler(filters.VIDEO, handle_video))
+    # Añadimos el manejador para archivos de tipo imagen
+    app.add_handler(MessageHandler(filters.PHOTO, handle_photo))
     # Iniciamos el bot
     app.run_polling()
