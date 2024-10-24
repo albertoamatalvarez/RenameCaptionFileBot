9b1ac2bad5d2e2dd9d7980ae4d789ee1397490ce
Alberto <albertoamatalvarez@gmail.com>
2024-10-24 12:53:28 +0200

    Revert new feature

--- handler/handler.py
+++ handler/handler.py
@@ -12,9 +12,3 @@ async def handle_video(update, context):
     video = update.message.video
     file_name = splitext(video.file_name)[0] if video.file_name else 'Sin nombre'
     await update.message.reply_video(video.file_id, caption=file_name)
-
-
-async def handle_photo(update, context):
-    photo = update.message.photo
-    file_name = splitext(photo.file_name)[0] if photo.file_name else 'Sin nombre'
-    await update.message.reply_photo(photo.file_id, caption=file_name)
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from decouple import config
 from telegram.ext import ApplicationBuilder, MessageHandler, filters, CommandHandler
 
-from handler.handler import start, handle_video, handle_photo
+from handler.handler import start, handle_video
 
 # Define tu token de bot
 TOKEN = config('TELEGRAM_TOKEN')
@@ -13,7 +13,5 @@ if __name__ == '__main__':
     app.add_handler(CommandHandler('start', start))
     # Añadimos el manejador para archivos de tipo video
     app.add_handler(MessageHandler(filters.VIDEO, handle_video))
-    # Añadimos el manejador para archivos de tipo imagen
-    app.add_handler(MessageHandler(filters.PHOTO, handle_photo))
     # Iniciamos el bot
     app.run_polling()
